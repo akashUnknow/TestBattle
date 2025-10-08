339f2fccf15bee0d013c8eebe877a38b554e41c7
akashUnknow <akash.unknow@gmail.com>
2025-10-08 06:52:33 +0000

    ins

---
+++ client/app/Pages/TestInstractionDetails.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TestInstractionDetails = () => {
+  return (
+    <View>
+      <Text>TestInstractionDetails</Text>
+    </View>
+  )
+}
+
+export default TestInstractionDetails
